a0265c5a3e11e8853c2e0a342d7132c65334d1c8
Zihad550 <dev.jehadhossain@gmail.com>
2022-10-21 15:39:01 +0600

    fix: use diff package manager & update dev script & update db uri

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const port = process.env.PORT || 8000;
 
 
 // mongo db use and password and client
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bhmov.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = process.env.URI;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
